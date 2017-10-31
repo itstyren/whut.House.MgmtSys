73b351fa6caec7c9594000c0e5754874454dcd64
Terry-Ren <620461@qq.com>
2017-10-31 14:40:25 +0800

    2017.10.31 完成了部分部门参数的前端页面功能

--- code/whutHouseMgmtSysWeb/src/api/api.js
+++ code/whutHouseMgmtSysWeb/src/api/api.js
@@ -3,5 +3,11 @@ import axios from 'axios'
 let base = 'http://localhost:8787/whutHouseMgmtReposity/'
 
 // 参数设置--职工参数
-//
-export const getStaffDept = params => { return axios.get(`${base}getEmployeeParameter/5`, { params: params }) }
+// 获取方法
+export const getStaffParam = (params, paramClass) => { return axios.get(`${base}staffParam/get/${paramClass}`, { params: params }) }
+// 删除方法
+export const deleteStaffParam = (params, staffParamId) => { return axios.delete(`${base}staffParam/delete/${staffParamId}`, { params: params }) }
+// 新增方法
+export const postStaffParam = (params, paramClass) => { return axios.post(`${base}staffParam/add/${paramClass}`, params) }
+// 编辑方法
+export const putStaffParam = (params, staffParamId) => { return axios.put(`${base}staffParam/modify/${staffParamId}`, params) }
