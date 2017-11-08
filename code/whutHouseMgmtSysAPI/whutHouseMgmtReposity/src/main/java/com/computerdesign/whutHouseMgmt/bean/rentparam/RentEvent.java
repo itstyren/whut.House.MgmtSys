706b8fb1c131a1b4a1c6ca31d42080c1b83aaefd
HackXieHao <442667228@qq.com>
2017-11-08 11:23:12 +0800

    2017.11.8 优化日期类型的转换，改变前后端返回的Json数据

--- code/whutHouseMgmtSysAPI/whutHouseMgmtReposity/src/main/java/com/computerdesign/whutHouseMgmt/bean/rentparam/RentEvent.java
+++ code/whutHouseMgmtSysAPI/whutHouseMgmtReposity/src/main/java/com/computerdesign/whutHouseMgmt/bean/rentparam/RentEvent.java
@@ -2,6 +2,10 @@ package com.computerdesign.whutHouseMgmt.bean.rentparam;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class RentEvent {
     private Integer rentEventId;
 
@@ -11,8 +15,12 @@ public class RentEvent {
 
     private Boolean rentIsOpenSel;
 
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8") 
     private Date rentTimeBegin;
 
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8") 
     private Date rentTimeRanges;
 
     private String rentSelValReq;
