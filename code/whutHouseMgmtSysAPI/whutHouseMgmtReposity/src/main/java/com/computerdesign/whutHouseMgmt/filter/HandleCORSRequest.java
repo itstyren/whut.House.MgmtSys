68150bf1558170fa952965e79d7af40b94931b37
xhwanhaoran <whr1314520@qq.com>
2018-03-06 15:24:28 +0800

    2018.3.6 修改filter，method增加OPTIONS

---
+++ code/whutHouseMgmtSysAPI/whutHouseMgmtReposity/src/main/java/com/computerdesign/whutHouseMgmt/filter/HandleCORSRequest.java
@@ -0,0 +1,28 @@
+package com.computerdesign.whutHouseMgmt.filter;
+
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author wanhaoran
+ * @date 2018年3月6日 下午3:09:31
+ * 
+ */
+
+
+public class HandleCORSRequest {
+	@OPTIONS
+	@Path("/*")
+	public Response handleCORSRequest(@HeaderParam("Access-Control-Request-Method") final String requestMethods,
+	        @HeaderParam("Access-Control-Request-Headers") final String requestHeaders) throws Exception {
+	    Response.ResponseBuilder builder = Response.ok();
+	    if (requestHeaders != null)
+	        builder.header("Access-Control-Allow-Headers", requestHeaders);
+	    if (requestMethods != null)
+	        builder.header("Access-Control-Allow-Methods", requestMethods);
+	    return builder.build();
+	}
+}
