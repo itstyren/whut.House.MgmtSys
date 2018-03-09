b2982cdfd7af37b94ed1fa0cb3a5c5068b39cb65
xhwanhaoran <whr1314520@qq.com>
2018-03-09 15:29:25 +0800

    2018.3.9 修改登录员工状态：冻结与否，用AccountStatus表示

--- code/whutHouseMgmtSysAPI/whutHouseMgmtReposity/src/main/java/com/computerdesign/whutHouseMgmt/controller/user/UserLoginController.java
+++ code/whutHouseMgmtSysAPI/whutHouseMgmtReposity/src/main/java/com/computerdesign/whutHouseMgmt/controller/user/UserLoginController.java
@@ -20,15 +20,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.computerdesign.whutHouseMgmt.bean.Msg;
+import com.computerdesign.whutHouseMgmt.bean.staffmanagement.ViewStaff;
 import com.computerdesign.whutHouseMgmt.bean.token.Token;
 import com.computerdesign.whutHouseMgmt.bean.user.User;
 import com.computerdesign.whutHouseMgmt.bean.user.UserLogin;
-import com.computerdesign.whutHouseMgmt.bean.user.UserLoginReturn;
-import com.computerdesign.whutHouseMgmt.service.login.LoginService;
 import com.computerdesign.whutHouseMgmt.service.staffmanagement.StaffService;
+import com.computerdesign.whutHouseMgmt.service.staffmanagement.ViewStaffService;
 import com.computerdesign.whutHouseMgmt.service.token.TokenService;
 import com.computerdesign.whutHouseMgmt.service.user.UserLoginService;
-import com.computerdesign.whutHouseMgmt.service.user.UserReturnService;
 import com.computerdesign.whutHouseMgmt.utils.DateUtil;
 import com.fasterxml.jackson.annotation.JsonFormat.Value;
 
@@ -38,110 +37,129 @@ import io.jsonwebtoken.Claims;
 @Controller
 public class UserLoginController {
 
-	//分钟*秒*毫秒
-	private Integer TimeLimit = 60*60*1000;
-	
-	@Autowired
-	private LoginService loginService;
-	
+	// 分钟*秒*毫秒
+	private Integer TimeLimit = 60 * 60 * 1000;
+
 	@Autowired
 	private TokenService tokenService;
-	
+
 	@Autowired
-	private UserReturnService  userReturnService;
-	
+	private ViewStaffService viewStaffService;
+
 	/**
 	 * 按照账户密码登陆登陆
+	 * 
 	 * @param userLogin
-	 * @return 
+	 * @return
 	 */
-	@RequestMapping(value = "login",method = RequestMethod.POST)
+	@RequestMapping(value = "login", method = RequestMethod.POST)
 	@ResponseBody
-	public Msg login(@RequestBody UserLogin userLogin,HttpServletRequest request,HttpServletResponse response){
-		
+	public Msg login(@RequestBody UserLogin userLogin, HttpServletRequest request, HttpServletResponse response) {
+
 		String no = userLogin.getNo();
 		String password = userLogin.getPassword();
-		Long roleId = Long.valueOf(userLogin.getRoleId()).longValue();
-		
-		userReturnService.getByNo(no);
-		List<UserLoginReturn> users = loginService.getLogin(no, password, roleId);
-		//判断登陆信息
-		if (users.isEmpty()) {
-			//如果信息不正确，返回失败
-			return Msg.error("请输入正确的信息");	
-		}else{
-			
-			UserLoginReturn user = users.get(0);
-			if (!user.getStatus().equals("active")) {
-				return Msg.error("该账号已冻结，请联系管理员解冻");
-			} else {
+		int roleId = userLogin.getRoleId();
 
-//				UserLoginReturn userLoginReturn = userReturnService.getByNo(no);
-//
-//				request.getSession().setAttribute("isLogin", "yes");
-//				response.addHeader("X-token", "222222");
-//				return Msg.success("登陆成功").add("data", userLoginReturn);
-				Date d = DateUtil.getAppointHour(new Date(), 3);
-				String date = DateUtil.formatDate(d);
-				String token = no+"_"+password+"_"+roleId+"_"+date;
-//				String tokenD = SubjectUtil.getInstance().createToken(no, DateUtil.getAppointDate(new Date(), 1));
-	            try {
-	            	byte[] sourceBs = token.getBytes("UTF-8");  
-		            byte[] targetBs = Base64.encodeBase64(sourceBs); 
-					token = new String(targetBs,"UTF-8");
-				} catch (UnsupportedEncodingException e) {
-				    throw new RuntimeException(e); 
-				} 
-				
-				return Msg.success().add("token", token);
-			}
+		List<ViewStaff> viewStaffs = viewStaffService.getByStaffNo(no);
+		// 判断登陆信息
+		if (viewStaffs.isEmpty()) {
+			return Msg.error("账号不存在");
 		}
+		ViewStaff viewStaff = viewStaffs.get(0);
+		if (viewStaff.getAccountStatus()) {
+			return Msg.error("该账号已冻结，请联系管理员解冻");
+		}
+		if (!viewStaff.getStaffPassword().equals(password) || viewStaff.getRoleId() != roleId) {
+			return Msg.error("信息有误");
+		}
+		Date d = DateUtil.getAppointHour(new Date(), 3);
+		String date = DateUtil.formatDate(d);
+		String token = no + "_" + password + "_" + roleId + "_" + date;
+		try {
+			byte[] sourceBs = token.getBytes("UTF-8");
+			byte[] targetBs = Base64.encodeBase64(sourceBs);
+			token = new String(targetBs, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+
+		return Msg.success().add("token", token);
+		// UserLoginReturn userLoginReturn = userReturnService.getByNo(no);
+		//
+		// request.getSession().setAttribute("isLogin", "yes");
+		// response.addHeader("X-token", "222222");
+		//
+		// String tokenD = SubjectUtil.getInstance().createToken(no,
+		// DateUtil.getAppointDate(new Date(), 1));
+
 	}
-	
+
 	/**
 	 * 登陆后获取用户信息
+	 * 
 	 * @param token
 	 * @param request
 	 * @param response
 	 * @return
 	 */
-	@RequestMapping(value = "tokenLogin",method = RequestMethod.GET)
+	@RequestMapping(value = "tokenLogin", method = RequestMethod.GET)
 	@ResponseBody
-	public Msg tokenLogin(@RequestParam(value = "token")String token,HttpServletRequest request,HttpServletResponse response){		
-		
+	public Msg tokenLogin(@RequestParam(value = "token") String token, HttpServletRequest request,
+			HttpServletResponse response) {
+
 		System.out.println(token);
-		//将token解码
+		// 将token解码
 		byte[] targetBs;
 		try {
 			targetBs = token.getBytes("UTF-8");
-			byte[] sourceBs = Base64.decodeBase64(targetBs);  
-	        token = new String(sourceBs,"UTF-8");
+			byte[] sourceBs = Base64.decodeBase64(targetBs);
+			token = new String(sourceBs, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
-			 throw new RuntimeException(e); 
+			throw new RuntimeException(e);
 		}
-		//根据解码后的token获取账户、密码和roleId
+		// 根据解码后的token获取账户、密码和roleId
 		String[] strArr = token.split("_");
+
 		try {
+			// 错误的token可能会导致无法求出以下三个参数
 			String no = strArr[0];
 			String password = strArr[1];
-			Long roleId =  Long.valueOf(strArr[2]).longValue();
-			
-			List<UserLoginReturn> users = loginService.getLogin(no, password, roleId);
-			if (users.isEmpty()) {
-				return Msg.error("该令牌已失效，请退出后重新登陆");
+			int roleId = Integer.parseInt(strArr[2]);
+			Date date = DateUtil.parseDate(strArr[3]);
+			Long pastHour = DateUtil.pastHour(date);
+			// if (pastHour <=2) {
+			// response.setStatus(401);
+			// return false;
+			// }
+
+			List<ViewStaff> viewStaffs = viewStaffService.getByStaffNo(no);
+			// 判断登陆信息
+			if (viewStaffs.isEmpty()) {
+				response.setStatus(401);
+				return Msg.error("该令牌已失效");
 			}
-			UserLoginReturn userLoginReturn = users.get(0);
-			return Msg.success().add("data", userLoginReturn);
-			
+			ViewStaff viewStaff = viewStaffs.get(0);
+			if (viewStaff.getAccountStatus()) {
+				response.setStatus(401);
+				return Msg.error("该令牌已失效");
+			}
+			if (!viewStaff.getStaffPassword().equals(password) || viewStaff.getRoleId() != roleId) {
+				response.setStatus(401);
+				return Msg.error("该令牌已失效");
+			}
+
+			System.out.println(no + "   " + password + "   " + roleId + "   " + date + "  " + pastHour);
+			return Msg.success().add("data", viewStaff);
 		} catch (Exception e) {
-			return Msg.error("该令牌已失效，请退出后重新登陆");
+			response.setStatus(401);
+			return Msg.error("该令牌已失效");
 		}
-		
+
 	}
-	
-	@RequestMapping(value = "logout",method = RequestMethod.GET)
+
+	@RequestMapping(value = "logout", method = RequestMethod.GET)
 	@ResponseBody
-	public Msg logout(HttpServletRequest request){
+	public Msg logout(HttpServletRequest request) {
 		String token = (String) request.getHeader("X-token");
 		return Msg.success("退出登陆");
 	}
