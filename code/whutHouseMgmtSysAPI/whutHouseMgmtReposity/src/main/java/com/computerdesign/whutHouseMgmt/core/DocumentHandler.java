3c926effd04d98ad269263d66d9f94bef7daac9f
xhwanhaoran <whr1314520@qq.com>
2018-03-18 17:22:57 +0800

    2018.3.18 导出word初步

---
+++ code/whutHouseMgmtSysAPI/whutHouseMgmtReposity/src/main/java/com/computerdesign/whutHouseMgmt/core/DocumentHandler.java
@@ -0,0 +1,75 @@
+package com.computerdesign.whutHouseMgmt.core;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+/**
+ *
+ * @author wanhaoran
+ * @date 2018年3月18日 下午3:20:15
+ * 
+ */
+public class DocumentHandler {
+
+	private Configuration configuration = null;
+
+	public DocumentHandler() {
+		configuration = new Configuration();
+		configuration.setDefaultEncoding("UTF-8");
+	}
+
+	public byte[] createDocArea(Map<String, Object> dataMap, String outFilePath, String fileName) throws Exception {
+//		 this.configuration.setClassForTemplateLoading(DocumentHandler.class, "D:\\");//第一种模板路径
+		System.out.println("---进入createDocArea---");
+		this.configuration.setDirectoryForTemplateLoading(new File("D:\\"));// 第二种模板路径
+		Template t = null;
+		File outFile = null;
+		byte[] bFile = null;
+		try {
+			t = this.configuration.getTemplate(fileName, "UTF-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		outFile = new File(outFilePath);
+		Writer w = null;
+		FileOutputStream fos = null;
+
+		try {
+			fos = new FileOutputStream(outFile);
+			OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+
+			w = new BufferedWriter(osw);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		try {
+			t.process(dataMap, w);
+			if (outFile != null) {
+				FileInputStream fis = new FileInputStream(outFile);
+				bFile = new byte[(int) outFile.length()];
+				fis.read(bFile);
+				fis.close();
+			}
+			System.out.println("--写入完成---");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			w.close();
+			fos.close();
+		}
+		return bFile;
+	}
+}
