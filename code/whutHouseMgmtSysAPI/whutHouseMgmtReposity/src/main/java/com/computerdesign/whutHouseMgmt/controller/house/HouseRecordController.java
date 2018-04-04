ab2ed75333c7a775144c490314d34864b16483ed
xhwanhaoran <whr1314520@qq.com>
2018-04-04 23:07:42 +0800

    2018.4.4 房屋统计

--- code/whutHouseMgmtSysAPI/whutHouseMgmtReposity/src/main/java/com/computerdesign/whutHouseMgmt/controller/house/HouseRecordController.java
+++ code/whutHouseMgmtSysAPI/whutHouseMgmtReposity/src/main/java/com/computerdesign/whutHouseMgmt/controller/house/HouseRecordController.java
@@ -30,100 +30,102 @@ import io.swagger.annotations.ApiOperation;
  * 
  */
 @RestController
-@RequestMapping(value="/houseRecord/")
-@Api(value = "房屋记录Controller",description ="房屋记录接口")
+@RequestMapping(value = "/houseRecord/")
+@Api(value = "房屋记录Controller", description = "房屋记录接口")
 public class HouseRecordController {
 
-	
 	@Autowired
 	private ViewHouseService viewhouseService;
 
 	@Autowired
 	private HouseRegisterSelectService houseRegisterSelectService;
-	
+
 	@Autowired
 	private CampusService campusService;
-	
-		
+
 	/**
 	 * 按照查询条件获取房屋类型统计
+	 * 
 	 * @param paramTypeId
 	 * @param houseAllSelectModel
 	 * @return
 	 */
-	@PostMapping(value="Content")
-	@ApiOperation(notes="获取房屋类型", value = "获取房屋类型")
-	public Msg getHouseRecordContent(@RequestParam("paramTypeId")Integer paramTypeId,
-			@RequestBody HouseAllSelectModel houseAllSelectModel){
+	@PostMapping(value = "Content")
+	@ApiOperation(notes = "获取房屋类型", value = "获取房屋类型")
+	public Msg getHouseRecordContent(@RequestParam("paramTypeId") Integer paramTypeId,
+			@RequestBody HouseAllSelectModel houseAllSelectModel) {
 		List<ViewHouse> listViewHouse = houseRegisterSelectService.getByAllMultiConditionQuery(houseAllSelectModel);
-		
+
 		List<String> listString = new ArrayList<>();
 		Map<String, Integer> map = new HashMap<>();
-		if (paramTypeId == 1) {			
-			for (ViewHouse viewHouse:listViewHouse) {
+		if (paramTypeId == 1) {
+			for (ViewHouse viewHouse : listViewHouse) {
 				if (!listString.contains(viewHouse.getTypeName())) {
 					listString.add(viewHouse.getTypeName());
 				}
 				if (!map.containsKey(viewHouse.getTypeName())) {
-					map.put(viewHouse.getTypeName(),0);
+					map.put(viewHouse.getTypeName(), 0);
 				}
-				map.put(viewHouse.getTypeName(), map.get(viewHouse.getTypeName())+1);
+				map.put(viewHouse.getTypeName(), map.get(viewHouse.getTypeName()) + 1);
 			}
 		}
 		if (paramTypeId == 2) {
-			for (ViewHouse viewHouse:listViewHouse) {
+			for (ViewHouse viewHouse : listViewHouse) {
 				if (!listString.contains(viewHouse.getLayoutName())) {
 					listString.add(viewHouse.getLayoutName());
 				}
 				if (!map.containsKey(viewHouse.getLayoutName())) {
-					map.put(viewHouse.getLayoutName(),0);
+					map.put(viewHouse.getLayoutName(), 0);
 				}
-				map.put(viewHouse.getLayoutName(),map.get(viewHouse.getLayoutName())+1);
+				map.put(viewHouse.getLayoutName(), map.get(viewHouse.getLayoutName()) + 1);
 			}
 		}
 		if (paramTypeId == 3) {
-			for (ViewHouse viewHouse:listViewHouse) {
+			for (ViewHouse viewHouse : listViewHouse) {
 				if (!listString.contains(viewHouse.getStatusName())) {
 					listString.add(viewHouse.getStatusName());
 				}
 				if (!map.containsKey(viewHouse.getStatusName())) {
-					map.put(viewHouse.getStatusName(),0);
+					map.put(viewHouse.getStatusName(), 0);
 				}
-				map.put(viewHouse.getStatusName(), map.get(viewHouse.getStatusName())+1);
+				map.put(viewHouse.getStatusName(), map.get(viewHouse.getStatusName()) + 1);
 			}
 		}
 		if (paramTypeId == 4) {
-			for (ViewHouse viewHouse:listViewHouse) {
+			for (ViewHouse viewHouse : listViewHouse) {
 				if (!listString.contains(viewHouse.getStructName())) {
 					listString.add(viewHouse.getStructName());
 				}
 				if (!map.containsKey(viewHouse.getStructName())) {
-					map.put(viewHouse.getStructName(),0);
+					map.put(viewHouse.getStructName(), 0);
 				}
-				map.put(viewHouse.getStructName(), map.get(viewHouse.getStructName())+1);
+				map.put(viewHouse.getStructName(), map.get(viewHouse.getStructName()) + 1);
 			}
 		}
-		List<Map<String, String>> listMap =new ArrayList<Map<String,String>>();
+		List<Map<String, String>> listMap = new ArrayList<Map<String, String>>();
 		for (Entry<String, Integer> entry : map.entrySet()) {
-			Map<String, String> mapAl =new HashMap<String, String>();
+			Map<String, String> mapAl = new HashMap<String, String>();
 			mapAl.put("name", entry.getKey());
 			mapAl.put("value", entry.getValue().toString());
 			listMap.add(mapAl);
 		}
 		return Msg.success("获取房屋统计").add("name", listString).add("content", listMap);
 	}
-	
+
 	/**
 	 * 获取各校区已入住和空闲的房屋数量
+	 * 
 	 * @param houseAllSelectModel
 	 * @return
 	 */
 	@PostMapping(value = "houseCampus")
-	public Msg getHouseCampus(@RequestBody HouseAllSelectModel houseAllSelectModel){
+	public Msg getHouseCampus(@RequestBody HouseAllSelectModel houseAllSelectModel) {
 		List<ViewHouse> listViewHouse = houseRegisterSelectService.getByAllMultiConditionQuery(houseAllSelectModel);
 		List<String> listString = new ArrayList<>();
 		Map<String, Object> map = new HashMap<>();
-		for (ViewHouse viewHouse:listViewHouse) {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		// 遍历每一个房屋
+		for (ViewHouse viewHouse : listViewHouse) {
 			if (!listString.contains(viewHouse.getCampusName())) {
 				listString.add(viewHouse.getCampusName());
 			}
@@ -131,18 +133,38 @@ public class HouseRecordController {
 				Map<String, Integer> mapForOne = new HashMap<>();
 				mapForOne.put("已入住", 0);
 				mapForOne.put("未入住", 0);
-				map.put(viewHouse.getCampusName(),mapForOne);
+				map.put(viewHouse.getCampusName(), mapForOne);
 			}
 			if (viewHouse.getStatus() == 24) {
 				@SuppressWarnings("unchecked")
 				Map<String, Integer> mapForOne = (Map<String, Integer>) map.get(viewHouse.getCampusName());
-				mapForOne.put("未入住", mapForOne.get("未入住")+1);
-			}else{
+				mapForOne.put("未入住", mapForOne.get("未入住") + 1);
+			} else {
 				@SuppressWarnings("unchecked")
 				Map<String, Integer> mapForOne = (Map<String, Integer>) map.get(viewHouse.getCampusName());
-				mapForOne.put("已入住", mapForOne.get("已入住")+1);
+				mapForOne.put("已入住", mapForOne.get("已入住") + 1);
 			}
 		}
-		return Msg.success().add("name", listString).add("data", map);
+		List<Map<String, Object>> listMap = new ArrayList<Map<String, Object>>();
+		for (Entry<String, Object> entry : map.entrySet()) {
+			HashMap<String, Integer> mmm = (HashMap<String, Integer>) entry.getValue();
+			List<Map<String, Object>> list2 = new ArrayList<>();
+			
+			Map<String, Object> mapAl1 = new HashMap<String, Object>();
+			mapAl1.put("name","已入住");
+			mapAl1.put("value", mmm.get("已入住"));
+			
+			Map<String, Object> mapAl2 = new HashMap<String, Object>();
+			mapAl2.put("name","未入住");
+			mapAl2.put("value", mmm.get("未入住"));
+			list2.add(mapAl1);
+			list2.add(mapAl2);
+			Map<String, Object> mapFinal = new HashMap<>();
+			mapFinal.put("name", entry.getKey());
+			mapFinal.put("data", list2);
+			
+			listMap.add(mapFinal);
+		}
+		return Msg.success().add("name", listString).add("data", listMap);
 	}
 }
