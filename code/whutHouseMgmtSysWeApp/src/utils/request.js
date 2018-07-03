ff1fff0e6cded75ada51029f6e6903c54af5dc45
Terry-Ren <620461@qq.com>
2018-07-03 12:42:54 +0800

    2018.07.03 remote address change

--- code/whutHouseMgmtSysWeApp/src/utils/request.js
+++ code/whutHouseMgmtSysWeApp/src/utils/request.js
@@ -5,8 +5,8 @@ import store from "../store";
 //配置请求基地址
 fly.config = {
   // baseURL: "http://localhost:8787/whutHouseMgmtReposity",
-  // baseURL: 'http://www.terryren.com:8080/whutHouseMgmtReposity', // api的base_url
-  baseURL: 'http://120.78.226.24:8080/whutHouseMgmtReposity', // api的base_url
+  baseURL: 'http://www.terryren.com/whutHouseMgmtReposity', // api的base_url
+  // baseURL: 'http://120.78.226.24:8080/whutHouseMgmtReposity', // api的base_url
   timeout: '5000',
 }
 
